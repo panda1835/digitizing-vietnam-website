54535fbd280e4f7d00f98f8e49afeb9c9276559c
panda1835 <lehoangphuc1820@gmail.com>
2024-05-08 16:45:43 +0700

    add main app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./tailwind.css";
 
