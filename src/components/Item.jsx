9e88fcf8b46025b7a25667c3df9a4c0b3029cedd
panda1835 <lehoangphuc1820@gmail.com>
2024-05-14 07:47:53 +0700

    add item component

---
+++ src/components/Item.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Item = (props) => {
+  return (
+    <Link to={props.link}>
+      <div className="">
+        <img
+          src={props.imageUrl}
+          alt={props.title}
+          className="object-cover rounded-lg w-full h-40"
+        />
+        <div className="text-xl text-primary-blue mt-2">{props.title}</div>
+        <p className="text-gray-500">{props.description}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Item;
