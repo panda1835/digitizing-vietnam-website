3eebbe525fd3a055e620ab77cebffff9c69cb85a
panda1835 <lehoangphuc1820@gmail.com>
2024-05-17 10:46:46 +0700

    factor into 3 api calls rather than 1

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -6,78 +6,55 @@ import Item from "../components/Item";
 import config from "../config";
 
 const Blogs = () => {
-  const [blogs, setBlogs] = useState({});
+  const [news, setNews] = useState([]);
+  const [highlights, setHighlights] = useState([]);
+  const [initiatives, setInitiatives] = useState([]);
   const [loadingBlogs, setLoadingBlogs] = useState(true);
 
   useEffect(() => {
-    fetch(config["api"]["blogs"])
+    fetch(`${config["api"]["blogs"]}/news`)
       .then((response) => response.json())
       .then((data) => {
-        setBlogs(data);
+        setNews(data["data"]);
         setLoadingBlogs(false);
       })
-      .catch(() => {
-        setBlogs({
-          news: [
-            {
-              id: "news-1",
-              title: "News 1",
-              imageUrl: "https://via.placeholder.com/500",
-            },
-            {
-              id: "news-2",
-              title: "News 2",
-              imageUrl: "https://via.placeholder.com/500",
-            },
-            {
-              id: "news-3",
-              title: "News 3",
-              imageUrl: "https://via.placeholder.com/500",
-            },
-          ],
-          highlights: [
-            {
-              id: "highlight-1",
-              title: "Highlight 1",
-              imageUrl: "https://via.placeholder.com/500",
-            },
-            {
-              id: "highlight-2",
-              title: "Highlight 2",
-              imageUrl: "https://via.placeholder.com/500",
-            },
-            {
-              id: "highlight-3",
-              title: "Highlight 3",
-              imageUrl: "https://via.placeholder.com/500",
-            },
-          ],
-          initiatives: [
-            {
-              id: "initiative-1",
-              title: "Initiative 1",
-              imageUrl: "https://via.placeholder.com/500",
-            },
-            {
-              id: "initiative-2",
-              title: "Initiative 2",
-              imageUrl: "https://via.placeholder.com/500",
-            },
-            {
-              id: "initiative-3",
-              title: "Initiative 3",
-              imageUrl: "https://via.placeholder.com/500",
-            },
-          ],
-        });
+      .catch((e) => {
+        console.error(e);
         setLoadingBlogs(false);
       });
-  });
+  }, []);
+
+  useEffect(() => {
+    fetch(`${config["api"]["blogs"]}/highlights`)
+      .then((response) => response.json())
+      .then((data) => {
+        setHighlights(data["data"]);
+        setLoadingBlogs(false);
+      })
+      .catch((e) => {
+        console.error(e);
+        setLoadingBlogs(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`${config["api"]["blogs"]}/initiatives`)
+      .then((response) => response.json())
+      .then((data) => {
+        setInitiatives(data["data"]);
+        setLoadingBlogs(false);
+      })
+      .catch((e) => {
+        console.error(e);
+        setLoadingBlogs(false);
+      });
+  }, []);
+
   return (
     <div className="flex flex-col max-width">
       <div className="flex-col mb-20 mx-5">
         {/* Header */}
-        <section className="flex-col text-center">
+        <section className="flex-col text-center mb-10">
           <h1>Vietnam Digitized: A Blog</h1>
           <p className="text-gray-500">
             News, insights and initiatives on the digital front of Vietnamese
@@ -99,9 +76,8 @@ const Blogs = () => {
         <section className="mb-10">
           <h1>News</h1>
           <div className="grid grid-cols-3 gap-8 mt-10">
-            {blogs &&
-              blogs["news"] &&
-              blogs["news"].map((item) => (
+            {news &&
+              news.map((item) => (
                 <Item
                   title={item.title}
                   description={""}
@@ -117,9 +93,8 @@ const Blogs = () => {
         <section className="mb-10">
           <h1>Highlights</h1>
           <div className="grid grid-cols-3 gap-8 mt-10">
-            {blogs &&
-              blogs["highlights"] &&
-              blogs["highlights"].map((item) => (
+            {highlights &&
+              highlights.map((item) => (
                 <Item
                   title={item.title}
                   description={""}
@@ -135,9 +110,8 @@ const Blogs = () => {
         <section className="mb-10">
           <h1>Initiatives</h1>
           <div className="grid grid-cols-3 gap-8 mt-10">
-            {blogs &&
-              blogs["initiatives"] &&
-              blogs["initiatives"].map((item) => (
+            {initiatives &&
+              initiatives.map((item) => (
                 <Item
                   title={item.title}
                   description={""}
