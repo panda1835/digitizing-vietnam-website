eae17f728174de9f7f0ecde587d34e3a83153a43
panda1835 <lehoangphuc1820@gmail.com>
2024-06-03 10:36:57 +0700

    remove base for deploying to netlify

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
-  base: "/digitizing-vietnam-frontend/",
+  // base: "/digitizing-vietnam-frontend/",
 });
