f71fadd331e7803ebe5a32f592cb95846bf403df
panda1835 <lehoangphuc1820@gmail.com>
2024-06-04 11:29:37 +0700

    use Modal for the pop up modal

--- src/pages/EachCollection.jsx
+++ src/pages/EachCollection.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
 
 import BookItem from "../components/BookItem";
 import Item from "../components/Item";
@@ -12,17 +13,17 @@ const EachCollection = () => {
   const [featuredArticles, setFeaturedArticles] = useState([]);
   const [loadingCollectionData, setLoadingCollectionData] = useState(true);
   const [loadingFeaturedArticles, setLoadingFeaturedArticles] = useState(true);
-  const [isLearnMoreOpen, setIsLearnMoreOpen] = useState(false);
-
-  const handleLearnMoreClick = () => {
-    setIsLearnMoreOpen(true);
-  };
+  const [modalIsOpen, setModalIsOpen] = React.useState(false);
 
   const handleLoadMoreClick = () => {};
 
-  const handleOverlayClose = () => {
-    setIsLearnMoreOpen(false);
-  };
+  function openLearnMoreModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeLearnMoreModal() {
+    setModalIsOpen(false);
+  }
 
   useEffect(() => {
     fetch(`${config["api"]["collections"]}/${collectionId}`)
@@ -64,51 +65,45 @@ const EachCollection = () => {
           </p>
           {/* Learn more button */}
           <div className="flex flex-row justify-center my-5">
-            <button className="" onClick={handleLearnMoreClick}>
+            <button className="" onClick={openLearnMoreModal}>
               Learn More
             </button>
           </div>
           {/* Information overlay */}
-          {isLearnMoreOpen && (
-            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-            <div
-              className="overlay fixed inset-0 bg-black bg-opacity-50 flex items-center"
-              onClick={handleOverlayClose}
-            >
-              <div
-                className="bg-white p-5 mx-10 rounded-lg shadow-lg text-left relative h-[80vh] overflow-auto"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <button
-                  onClick={handleOverlayClose}
-                  className="absolute top-0 right-0 m-2 bg-white text-primary-blue"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-                <p className="px-10 pt-5 items-start justify-start">
-                  {collectionData["information"]}
-                </p>
-              </div>
-            </div>
-          )}
         </section>
+
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeLearnMoreModal}
+          contentLabel="Example Modal"
+        >
+          <button
+            onClick={closeLearnMoreModal}
+            className="absolute top-0 right-0 m-2 bg-white text-primary-blue"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+          <p className="px-10 pt-5 items-start justify-start">
+            {collectionData["information"]}
+          </p>
+        </Modal>
 
         {/* Search bar */}
         <SearchBar />
 
         {/* Loading indicator */}
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mt-20">
           <div
             className={`loader ${
               loadingCollectionData || loadingFeaturedArticles
