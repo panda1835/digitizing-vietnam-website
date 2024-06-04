ed9970e02bf4a6e1a3ac79d3a5933d24d695a0a7
panda1835 <lehoangphuc1820@gmail.com>
2024-06-04 11:27:46 +0700

    add endpoints for online resources

--- src/config.js
+++ src/config.js
@@ -7,6 +7,7 @@ const config = {
     ocr: `${REACT_APP_BACKEND_URI}/ocr`,
     collections: `${REACT_APP_BACKEND_URI}/collections`,
     blogs: `${REACT_APP_BACKEND_URI}/blogs`,
+    onlineResources: `${REACT_APP_BACKEND_URI}/online-resources`,
   },
 };
 
