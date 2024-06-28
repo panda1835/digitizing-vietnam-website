754895d7565f54ff72edf90a3ea11735a587bc97
panda1835 <lehoangphuc1820@gmail.com>
2024-06-28 16:51:31 +0700

    make the search bar spans the whole width in mobile

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,14 +4,14 @@ import { useTranslation } from "react-i18next";
 const SearchBar = () => {
   const { t } = useTranslation();
   return (
-    <form className="flex mt-5 mb-15">
-      <div className="relative">
+    <form className="flex mt-5 mb-15 w-full">
+      <div className="relative flex-grow md:flex-grow-0">
         <MagnifyingGlassIcon className="absolute left-2 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500" />
         <input
           type="text"
           name="search-query"
           placeholder={t("btn-search")}
-          className="rounded border pl-8 md:w-80 h-10"
+          className="rounded border pl-8 w-full md:w-80 h-10"
         />
       </div>
       <input
