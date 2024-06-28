135c020c85467ae5629eabe1f42b0049f9f713b6
panda1835 <lehoangphuc1820@gmail.com>
2024-06-28 16:04:23 +0700

    make the home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,67 +7,64 @@ const Home = () => {
 
   return (
     <div className="flex flex-col items-center max-width">
-      {/* Header */}
-      <section className="flex-col text-center mb-20 mx-5">
-        <h1>Digitizing Vietnam</h1>
-        <p className="text-gray-500">{t("home-welcome-subtitle")}</p>
-      </section>
+      <div className="flex-col mb-20 px-5 w-full">
+        {/* Header */}
+        <section className="flex-col text-center mb-10">
+          <h1>Digitizing Vietnam</h1>
+          <p className="text-gray-500">{t("home-welcome-subtitle")}</p>
+        </section>
 
-      <section className="flex flex-row w-full justify-around items-center mb-10">
-        <div>
-          <h2>{t("home-about-digitizing-vietnam")}</h2>
-          <p className="w-96 mb-5">
-            {t("home-about-digitizing-vietnam-content")}
-          </p>
-          <Link to="/about-us" className="button">
-            {t("btn-learn-more")}
-          </Link>
-        </div>
+        <section className="flex flex-col md:flex-row justify-evenly items-center my-5">
+          <div className="md:w-96">
+            <img
+              className="my-5"
+              src="https://www.lrsoc.com/web/wp-content/uploads/2021/01/Placeholder-shop.jpg"
+              alt="About Digitizing Vietnam"
+            ></img>
+          </div>
+          <div className="md:w-96">
+            <h2>{t("home-about-digitizing-vietnam")}</h2>
+            <p className="mb-5">{t("home-about-digitizing-vietnam-content")}</p>
+            <Link to="/about-us" className="button">
+              {t("btn-learn-more")}
+            </Link>
+          </div>
+        </section>
 
-        <div>
-          <img
-            className="w-96"
-            src="https://www.lrsoc.com/web/wp-content/uploads/2021/01/Placeholder-shop.jpg"
-            alt="About Digitizing Vietnam"
-          ></img>
-        </div>
-      </section>
+        <section className="flex flex-col md:flex-row justify-evenly items-center my-5">
+          <div className="md:w-96 order-1 md:order-2">
+            <img
+              className="my-5"
+              src="https://digitizing-vietnam.s3.ap-southeast-1.amazonaws.com/assets/Home+Page+2.jpg"
+              alt="Our Collections"
+            ></img>
+          </div>
+          <div className="md:w-96 order-2 md:order-1">
+            <h2>{t("home-our-collections")}</h2>
+            <p className="mb-5">{t("home-our-collections-content")}</p>
+            <Link to="/our-collections" className="button">
+              {t("btn-learn-more")}
+            </Link>
+          </div>
+        </section>
 
-      <section className="flex flex-row w-full justify-around items-center mb-10">
-        <div>
-          <img
-            className="w-96"
-            src="https://digitizing-vietnam.s3.ap-southeast-1.amazonaws.com/assets/Home+Page+2.jpg"
-            alt="Our Collections"
-          ></img>
-        </div>
-
-        <div>
-          <h2>{t("home-our-collections")}</h2>
-          <p className="w-96 mb-5">{t("home-our-collections-content")}</p>
-          <Link to="/our-collections" className="button">
-            {t("btn-learn-more")}
-          </Link>
-        </div>
-      </section>
-
-      <section className="flex flex-row w-full justify-around items-center mb-10">
-        <div>
-          <h2>{t("home-our-blog")}</h2>
-          <p className="w-96 mb-5">{t("home-our-blog-content")}</p>
-          <Link to="/blogs" className="button">
-            {t("btn-learn-more")}
-          </Link>
-        </div>
-
-        <div>
-          <img
-            className="w-96"
-            src="https://www.lrsoc.com/web/wp-content/uploads/2021/01/Placeholder-shop.jpg"
-            alt="Our Blog"
-          ></img>
-        </div>
-      </section>
+        <section className="flex flex-col md:flex-row justify-evenly items-center my-5">
+          <div className="md:w-96">
+            <img
+              className="my-5"
+              src="https://www.lrsoc.com/web/wp-content/uploads/2021/01/Placeholder-shop.jpg"
+              alt="Our Blog"
+            ></img>
+          </div>
+          <div className="md:w-96">
+            <h2>{t("home-our-blog")}</h2>
+            <p className="mb-5">{t("home-our-blog-content")}</p>
+            <Link to="/blogs" className="button">
+              {t("btn-learn-more")}
+            </Link>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
