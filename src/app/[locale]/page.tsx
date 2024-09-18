c740e18b9df9af64e9493d6f140fae3c224f934e
panda1835 <lehoangphuc1820@gmail.com>
2024-09-18 17:04:40 +0700

    fix: import

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 import { useTranslations } from "next-intl";
-import { Link } from "@/i18n/routing";
+import { Link } from "../../i18n/routing";
 
 import ImageSlideshow from "../../components/ImageSlideshow";
 const Home = ({ params: { locale } }) => {
