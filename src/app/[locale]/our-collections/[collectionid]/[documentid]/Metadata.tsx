c726b29d8947f0b6e64465c3c9728e4ccc2d079d
panda1835 <lehoangphuc1820@gmail.com>
2025-02-27 17:14:08 +0700

    feat: update metadata order

--- src/app/[locale]/our-collections/[collectionid]/[documentid]/Metadata.tsx
+++ src/app/[locale]/our-collections/[collectionid]/[documentid]/Metadata.tsx
@@ -2,58 +2,13 @@ export default function Metadata({ locale, collectionItemData }) {
   return (
     <div>
       {/* Title */}
-      <div className="grid grid-cols-2 mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 mt-8">
         <div className="items-center gap-3">
           <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
             {locale === "en" ? "Title" : "Tiêu đề"}:
           </div>
           <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
             {collectionItemData.title}
-          </div>
-        </div>
-        {/* Edition */}
-        <div className="items-center gap-3 mt-4">
-          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
-            {locale === "en" ? "Edition" : "Phiên bản"}:
-          </div>
-          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
-            {collectionItemData.edition}
-          </div>
-        </div>
-        {/* Identifier */}
-        <div className="items-center gap-3 mt-4">
-          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
-            {locale === "en" ? "Identifier" : "Mã định danh"}:
-          </div>
-          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
-            {collectionItemData.identifier || "N/A"}
-          </div>
-        </div>
-        {/* URL */}
-        <div className="items-center gap-3 mt-4">
-          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
-            {locale === "en" ? "URL" : "Liên kết"}:
-          </div>
-          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
-            {collectionItemData.url || "N/A"}
-          </div>
-        </div>
-        {/* Note */}
-        <div className="items-center gap-3 mt-4">
-          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
-            {locale === "en" ? "Note" : "Ghi chú"}:
-          </div>
-          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
-            {collectionItemData.note || "N/A"}
-          </div>
-        </div>
-        {/* Related Works */}
-        <div className="items-center gap-3 mt-4">
-          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
-            {locale === "en" ? "Related Works" : "Tác phẩm liên quan"}:
-          </div>
-          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
-            {collectionItemData.related_works || "N/A"}
           </div>
         </div>
         {/* Resource Types */}
@@ -137,15 +92,6 @@ export default function Metadata({ locale, collectionItemData }) {
               .join(", ") || "N/A"}
           </div>
         </div>
-        {/* Access Condition */}
-        <div className="items-center gap-3 mt-4">
-          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
-            {locale === "en" ? "Access Condition" : "Điều kiện truy cập"}:
-          </div>
-          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
-            {collectionItemData.access_condition?.name || "N/A"}
-          </div>
-        </div>
         {/* Publisher */}
         <div className="items-center gap-3 mt-4">
           <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
@@ -153,6 +99,61 @@ export default function Metadata({ locale, collectionItemData }) {
           </div>
           <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
             {collectionItemData.publisher?.name || "N/A"}
+          </div>
+        </div>
+        {/* Edition */}
+        <div className="items-center gap-3 mt-4">
+          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
+            {locale === "en" ? "Edition" : "Phiên bản"}:
+          </div>
+          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
+            {collectionItemData.edition}
+          </div>
+        </div>
+        {/* Identifier */}
+        <div className="items-center gap-3 mt-4">
+          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
+            {locale === "en" ? "Identifier" : "Mã định danh"}:
+          </div>
+          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
+            {collectionItemData.identifier || "N/A"}
+          </div>
+        </div>
+        {/* URL */}
+        <div className="items-center gap-3 mt-4">
+          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
+            {locale === "en" ? "URL" : "Liên kết"}:
+          </div>
+          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
+            {collectionItemData.url || "N/A"}
+          </div>
+        </div>
+        {/* Note */}
+        <div className="items-center gap-3 mt-4">
+          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
+            {locale === "en" ? "Note" : "Ghi chú"}:
+          </div>
+          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
+            {collectionItemData.note || "N/A"}
+          </div>
+        </div>
+        {/* Related Works */}
+        <div className="items-center gap-3 mt-4">
+          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
+            {locale === "en" ? "Related Works" : "Tác phẩm liên quan"}:
+          </div>
+          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
+            {collectionItemData.related_works || "N/A"}
+          </div>
+        </div>
+
+        {/* Access Condition */}
+        <div className="items-center gap-3 mt-4">
+          <div className="text-[#777777] text-lg font-normal font-['Helvetica Neue']">
+            {locale === "en" ? "Access Condition" : "Điều kiện truy cập"}:
+          </div>
+          <div className="text-branding-black text-base font-light font-['Helvetica Neue']">
+            {collectionItemData.access_condition?.name || "N/A"}
           </div>
         </div>
       </div>
