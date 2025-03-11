e96d1375c655c04cfb2521f7676fb95639fde158
panda1835 <lehoangphuc1820@gmail.com>
2025-03-11 17:14:18 +0700

    feat: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import { getTranslations } from "next-intl/server";
+import { Link } from "@/i18n/routing";
+import { Merriweather } from "next/font/google";
+
+const merriweather = Merriweather({ weight: "300", subsets: ["vietnamese"] });
+
+const NotFound = async ({ params: { locale } }) => {
+  const t = await getTranslations();
+
+  return (
+    <div className="flex flex-col items-center max-width">
+      <div
+        className={`${merriweather.className} text-branding-black text-4xl mt-20`}
+      >
+        {t("ErrorPage.not-found")}
+      </div>
+      <div className="mt-10 text-muted-foreground">
+        {t("ErrorPage.not-found-message")}
+      </div>
+      <Link href={`/`}>
+        <div className="mt-10 text-branding-white border bg-branding-black px-4 py-2 rounded-md cursor-pointer">
+          {t("ErrorPage.back-to-home")}
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
