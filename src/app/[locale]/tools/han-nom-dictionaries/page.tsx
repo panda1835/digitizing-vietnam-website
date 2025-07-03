ecaa1fa39057cbe6e5c2cccb990b956f4b139a06
panda1835 <lehoangphuc1820@gmail.com>
2025-07-03 18:40:29 +0700

    feat: add pages for NT QATD

--- src/app/[locale]/tools/han-nom-dictionaries/page.tsx
+++ src/app/[locale]/tools/han-nom-dictionaries/page.tsx
@@ -38,6 +38,17 @@ const HanNomDictionaries = async ({ params: { locale } }) => {
       target: "",
       rel: "",
     },
+    // {
+    //   href: "/tools/han-nom-dictionaries/nguyen-trai-quoc-am-tu-dien",
+    //   title: t(
+    //     "Tools.han-nom-dictionaries.dictionaries.nguyen-trai-quoc-am-tu-dien.name"
+    //   ),
+    //   description: t(
+    //     "Tools.han-nom-dictionaries.dictionaries.nguyen-trai-quoc-am-tu-dien.description"
+    //   ),
+    //   target: "",
+    //   rel: "",
+    // },
   ];
 
   return (
