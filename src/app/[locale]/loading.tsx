2d8aea9385402fc8ac6151371564b2f21081ca2f
panda1835 <lehoangphuc1820@gmail.com>
2026-02-27 22:44:05 +0700

    feat: update loading skeleton in mobile to hide footer below

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,8 +1,8 @@
 import LoadingSpinner from "@/components/layout/LoadingSpinner";
 const Loading = () => {
   return (
-    <div className="flex flex-col max-width justify-center items-center">
-      <div className="mt-20">
+    <div className="flex flex-col max-width items-center justify-start pt-24 min-h-screen lg:min-h-0 lg:justify-center lg:pt-0">
+      <div className="lg:mt-20">
         <LoadingSpinner />
       </div>
     </div>
